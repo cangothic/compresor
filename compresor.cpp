f8ae1005b6f4eed04a60b879b5cf73cce939c546
cangothic <rockangel2910@gmail.com>
2016-03-10 00:37:43 -0500

    organizando en metodos
    XD

--- compresor.cpp
+++ compresor.cpp
@@ -3,14 +3,28 @@
 #include <string>
 #include <map>
 using namespace std;
+
 size_t numeroDeBitsagrupar;
-map<string,int> diccionarioContarPalabra;
+map<string,int> contarPalabras(size_t tamanoDePalabras,string archivo);
+
 int main() {
   cout << "ingrese el numero de caracteres a agrupar" <<endl;
   cin>>numeroDeBitsagrupar;
+  map<string,int> diccionarioContarPalabra=contarPalabras(numeroDeBitsagrupar,"casos/in1.txt");
+  string prueba ="escribir";
+  ofstream file("out.txt",ofstream::out);
+  for(size_t i=0;i<prueba.size();i++){
+    file<<prueba[i];
+  }
+  file.close();
+  return 0;
+}
+map<string,int>contarPalabras(size_t tamanoDePalabras,string archivo){
+  map<string,int> diccionarioContarPalabra;
   string palabra="";
-  for (fstream file("in1.txt");file.good();) {
-    for (size_t indCaracter = 0; indCaracter < numeroDeBitsagrupar; indCaracter++) {
+  fstream file(archivo.c_str());
+  while(file.good()) {
+    for (size_t indCaracter = 0; indCaracter < tamanoDePalabras; indCaracter++) {
       char caracter=file.get();
       palabra.push_back(caracter);
     }
@@ -19,11 +33,5 @@ int main() {
     }
     diccionarioContarPalabra[palabra]++;
   }
-  string prueba ="escribir";
-  ofstream file("out.txt",ofstream::out);
-  for(size_t i=0;i<prueba.size();i++){
-    file<<prueba[i];
-  }
-  file.close();
-  return 0;
+  return diccionarioContarPalabra;
 }
